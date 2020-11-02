845a00c4e86d37a4bd24ddffec09e0d18533eb3a
Mahi Rahman <50224585+starexify@users.noreply.github.com>
2020-11-02 21:59:21 +1100

    GET method
    - Added a GET method to validate credit card
    http://localhost:8080/CreditCardValidatorAPI/api/validate?Card_num=XXXXXXXXXXXXXXXX
    
    - Removed unused test files

--- src/creditCardValidator/Validate.java
+++ src/creditCardValidator/Validate.java
@@ -1,6 +1,31 @@
 package creditCardValidator;
 
-public class Validate {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
+@Path("/validate")
+public class Validate {
+	
+	private String resource;
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String isValid(@QueryParam("Card_num") String cardNum) {
+		System.out.println("Card is " + cardNum);
+        if (Luhn.validateCard(cardNum)) {
+            System.out.println("This is a valid card");
+        	resource = "<h1>Valid</h1>";
+        }
+        else {
+            System.out.println("This is not a valid card");
+			resource = "<h1>Invalid</h1>";
+        }
+		return resource;
+	}
 
 }
