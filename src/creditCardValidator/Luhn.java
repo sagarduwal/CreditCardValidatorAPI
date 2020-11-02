522105748cd114358859a2bff847d514e5028573
Mahi Rahman <50224585+starexify@users.noreply.github.com>
2020-11-03 01:51:29 +1100

    Comments/Documentation

--- src/creditCardValidator/Luhn.java
+++ src/creditCardValidator/Luhn.java
@@ -15,11 +15,13 @@ package creditCardValidator;
 
 public class Luhn {
 	
+	//Data types used in luhnAlgorithm()
 	private static int numOfDigits;
 	private static int currDigit;
 	private static int totalOdd;
 	private static int totalEven;
 	
+	//Constructor to initialize the total for odd and even weights
 	public Luhn() {
 		totalOdd = 0;
 		totalEven = 0;
@@ -32,16 +34,16 @@ public class Luhn {
 		for(int i = 0; i < numOfDigits; i++) {
 			currDigit = Character.digit(reverseCardNum.charAt(i), 10); //Get the current digit at index i
 			if(i % 2 == 0) {
-				totalOdd += currDigit;
+				totalOdd += currDigit;	//Adds current digit of weight '1' to total
 			}
 			else {
-				totalEven += currDigit * 2;
-                if(currDigit >= 5) {
-                	totalEven -= 9;
+				totalEven += currDigit * 2;	//Adds current digit of weight '2' to total
+                if(currDigit >= 5) {	//Checks if it is a double digit
+                	totalEven -= 9;		//If so then remove 9
                 }
 			}
 		}
-	    return ((totalOdd + totalEven) % 10 == 0);
+	    return ((totalOdd + totalEven) % 10 == 0);	//Return True/False if {total}MOD{10} is 0
 	}
 
 }
--- src/creditCardValidator/Validate.java
+++ src/creditCardValidator/Validate.java
@@ -1,5 +1,6 @@
 package creditCardValidator;
 
+//Requires dependencies located in /WebContent/WEB-INF/lib/...
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,13 +12,16 @@ import javax.ws.rs.core.Application;
 @Path("/validates")
 public class Validate {
 
+	//GET method that validates a credit card using an instance of the Luhn class 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String validateCard(@QueryParam("card_num") String cardNum) {
-		
-		System.out.println("GET method is called!");
-		
+	//eg) localhost:8080/CreditCardValidatorAPI/v1/validates?card_num={cardNum}
+
+		//System.out.println("GET method is called!");
 		Luhn checkLuhn = new Luhn();
+
+		//Returns response in JSON format
 		return "{"
 				+"\n"
 				+"	\"Card Number\": " + cardNum + ","
