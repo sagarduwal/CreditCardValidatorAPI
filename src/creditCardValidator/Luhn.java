fee17e3a3afeeb7e139f4e491c77051448b1ecb9
Mahi Rahman <50224585+starexify@users.noreply.github.com>
2020-11-04 02:12:25 +1100

    Better formatting of JSON response

--- src/creditCardValidator/Luhn.java
+++ src/creditCardValidator/Luhn.java
@@ -31,6 +31,7 @@ public class Luhn {
 	{
 		numOfDigits = cardNum.length(); //Retrieve the length of the raw card number
 		String reverseCardNum = new StringBuffer(cardNum).reverse().toString(); //Reverses the raw card number
+		
 		for(int i = 0; i < numOfDigits; i++) {
 			currDigit = Character.digit(reverseCardNum.charAt(i), 10); //Get the current digit at index i
 			if(i % 2 == 0) {
@@ -43,6 +44,7 @@ public class Luhn {
                 }
 			}
 		}
+		
 	    return ((totalOdd + totalEven) % 10 == 0);	//Return True/False if {total}MOD{10} is 0
 	}
 
